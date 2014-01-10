8c53c24d0c84c54169085a6141782fe9e2fcd8e5
Shane Gao <weigaox@gmail.com>
2014-01-10 17:57:57 +0800

    macros added

--- Helpers/QuickMethod.h
+++ Helpers/QuickMethod.h
@@ -10,13 +10,36 @@
 #import <sys/utsname.h>
 
 #define APP_VERSION [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"]
-#define IS_IPAD   UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
+
+#define WIDTH_IPAD 1024
+#define WIDTH_IPHONE_5 568
+#define WIDTH_IPHONE_4 480
+#define HEIGHT_IPAD 768
+#define HEIGHT_IPHONE 320
+
+#define IS_IPAD  UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
 #define IS_PHONE UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone
 
 #define IS_OS7_LATER ([[[UIDevice currentDevice] systemVersion] compare:@"7.0"] != NSOrderedAscending )
-#define IS_RETINA [[UIScreen mainScreen] respondsToSelector:@selector(scale)] && [[UIScreen mainScreen] scale] == 2
-#define IS_IPHONE UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone && [[[UIDevice currentDevice] model] hasPrefix:@"iPhone"]
-#define IS_IPOD UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPhone && [[[UIDevice currentDevice] model] hasPrefix:@"iPod"]
+#define IS_RETINA ([[UIScreen mainScreen] respondsToSelector:@selector(scale)] && [[UIScreen mainScreen] scale] == 2)
+#define IS_IPHONE (IS_PHONE && [[[UIDevice currentDevice] model] isEqualToString:@"iPhone"])
+#define IS_IPOD (IS_PHONE && [[[UIDevice currentDevice] model] isEqualToString:@"iPod touch"])
+
+#define IS_WIDESCREEN ( fabs( ( double )[ [ UIScreen mainScreen ] bounds ].size.height - ( double )568 ) < DBL_EPSILON )
+
+#define IS_PHONE_5  ( IS_PHONE && IS_WIDESCREEN )
+#define IS_IPHONE_5 ( IS_IPHONE && IS_WIDESCREEN )
+#define IS_IPOD_5   ( IS_IPHONE &% IS_WIDESCREEN )
+
+
+#define cp_ph4(__X__, __Y__) ccp(cx_ph4(__X__), cy_ph4(__Y__))
+#define cx_ph4(__X__) (IS_IPAD ? (__X__ * WIDTH_IPAD / WIDTH_IPHONE_4) : (IS_IPHONE_5 ? (__X__ * WIDTH_IPHONE_5 / WIDTH_IPHONE_4) : (__X__)))
+#define cy_ph4(__Y__) (IS_IPAD ? (__Y__ * HEIGHT_IPAD / HEIGHT_IPHONE) : (__Y__))
+
+#define cp_pad(__X__, __Y__) ccp(cx_pad(__X__), cy_pad(__Y__))
+#define cx_pad(__X__) (IS_IPAD ? (__X__) : (IS_IPHONE_5 ? (__X__ * WIDTH_IPHONE_5 / WIDTH_IPAD) : (__X__ * WIDTH_IPHONE_4 / WIDTH_IPAD)))
+#define cy_pad(__Y__) (IS_IPAD ? (__Y__) : (__Y__ * HEIGHT_IPHONE / HEIGHT_IPAD))
+
 
 extern NSString* const kCallNotSupportOnThisDevice;
 extern NSString* const kSmsNotSupportOnThisDevice;
--- SGCocoaHelpersDemo/SGCocoaHelpersDemo/AppDelegate.m
+++ SGCocoaHelpersDemo/SGCocoaHelpersDemo/AppDelegate.m
@@ -7,6 +7,19 @@
 //
 
 #import "AppDelegate.h"
+#import "LocalNotificationManager.h"
+#import "UIColor+Helpers.h"
+#import "UIView+Helpers.h"
+#import "NSDate+Helpers.h"
+#import "FileManager.h"
+#import "LocaleUtils.h"
+#import "LocationUtils.h"
+#import "MKMapView+Helpers.h"
+#import "PListManager.h"
+#import "QuickMethod.h"
+#import "Random.h"
+#import "NSString+Helpers.h"
+#import "UIImage+Helpers.h"
 
 @implementation AppDelegate
 
