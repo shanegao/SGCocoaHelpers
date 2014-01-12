1a61e06e28a69cbf89393cbc8ba345583b65d674
Shane Gao <weigaox@gmail.com>
2014-01-12 13:59:47 +0800

    update UIImage Helpers

--- Helpers/UIImage+Helpers.h
+++ Helpers/UIImage+Helpers.h
@@ -6,8 +6,14 @@
 //  Copyright (c) 2013年 touchmob.com. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import <Accelerate/Accelerate.h>
+#import <QuartzCore/QuartzCore.h>
 
 @interface UIImage (Helpers)
 + (UIImage *)imageWithImage:(UIImage *)image scaledToSize:(CGSize)newSize;
++ (UIImage *)imageWithColor:(UIColor *)color;
+- (UIImage*)blurredImage:(CGFloat)blurAmount;
+
++ (UIImage *)screenshot;
 @end
