8a6082e7c9d5b72d6908ab008d60e072590be4fe
Shane Gao <weigaox@gmail.com>
2014-06-07 11:30:08 +0800

    add ValidateUtils

---
+++ Helpers/ValidateUtils.h
@@ -0,0 +1,13 @@
+//
+//  .h
+//  TouchMobLib
+//
+//  Created by Shane Gao on 7/20/08.
+//  Copyright touchmob.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ValidateUtils : NSObject
+- (BOOL)validateMobile:(NSString *)mobileNum;
+@end
